c75b3f23ab480bab492ce62b270265b8ca4b4908
Gaspar <e972136@gmail.com>
2026-03-23 15:54:17 -0600

    Primero

---
+++ src/main/java/com/gaspar/report/SpringBootMySqlJasperreportApplication.java
@@ -0,0 +1,13 @@
+package com.gaspar.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMySqlJasperreportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMySqlJasperreportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaspar/report/SpringBootMySqlJasperreportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaspar.report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMySqlJasperreportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
